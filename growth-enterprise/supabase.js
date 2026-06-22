02246b28d4998159ef1037ff1c09307f6d12ee8b
thinkstoreve <thinkstore.ve@gmail.com>
2026-06-22 17:16:34 -0400

    Update supabase.js

--- growth-enterprise/supabase.js
+++ growth-enterprise/supabase.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://clhnndxsgzqnihhtrout.supabase.co/rest/v1/";
 const SUPABASE_ANON_KEY = "sb_publishable_Q7ynhCPp8nMFQywia1LqCQ_6UEAGqRZ";
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
