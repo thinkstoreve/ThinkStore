8b2c23a4bffed9d38473d9db393de8254b9486ac
thinkstoreve <thinkstore.ve@gmail.com>
2026-06-22 15:31:59 -0400

    Update app.js

--- growth-enterprise/app.js
+++ growth-enterprise/app.js
@@ -1,1 +1,7 @@
+const enterButton = document.getElementById("enterGrowth");
 
+if (enterButton) {
+  enterButton.addEventListener("click", () => {
+    window.location.href = "dashboard.html";
+  });
+}
